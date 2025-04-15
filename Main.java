ec2e54d7a9bfd4b12b8b3aed8945d458e4bf7805
Yakanov <elyadaj@gmail.com>
2025-04-15 15:01:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,17 +7,21 @@ public class Main {
         while (true) {
             System.out.println("\n--- MENU ---");
             System.out.println("1. Ajouter un produit");
-            System.out.println("2. Modifier un produit");
-            System.out.println("3. Supprimer un produit");
-            System.out.println("4. Ajouter un mouvement de stock");
-            System.out.println("5. Générer un rapport PDF");
-            System.out.println("6. Quitter");
+            System.out.println("2. afficher les Produits");
+            System.out.println("3. Modifier un produit");
+            System.out.println("4. Supprimer un produit");
+            System.out.println("5. Ajouter un mouvement de stock");
+            System.out.println("6. Générer un rapport PDF");
+            System.out.println("7. Quitter");
             System.out.print("Votre choix : ");
-            
+
+            String choixStr = scanner.nextLine();  // lire comme une chaîne
+            int choix;
+
             try {
                 choix = Integer.parseInt(choixStr);  // tenter de convertir en int
             } catch (NumberFormatException e) {
-                System.out.println("Entrée invalide. Veuillez entrer un nombre entre 1 et 6.");
+                System.out.println("Entrée invalide. Veuillez entrer un nombre entre 1 et 7.");
                 continue;
             }
 
@@ -26,18 +30,20 @@ public class Main {
                     GestionProduit.ajouterProduit();
                     break;
                 case 2:
+                    GestionProduit.afficherProduit();
+                case 3:
                     GestionProduit.modifierProduit();
                     break;
-                case 3:
+                case 4:
                     GestionProduit.supprimerProduit();
                     break;
-                case 4:
+                case 5:
                     GestionStock.ajouterMouvement();
                     break;
-                case 5:
+                case 6:
                     RapportStock.genererRapport();
                     break;
-                case 6:
+                case 7:
                     System.out.println("Fermeture du programme.");
                     scanner.close();
                     return;
